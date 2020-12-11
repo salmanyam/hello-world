ce0067f70605185d629313382b2916e9a5f53d90
Salman Ahmed <ahmedms@vt.edu>
2020-12-10 20:38:17 -0500

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,10 @@
+public RollingProximityId generateId(int intervalNumber) 
+    throws CryptoException 
+{
+    return new RollingProximityId(
+        encryptor.encrypt(
+            generatePaddedData(
+                intervalNumber, 
+                aesPadding)), 
+        true);
+}
